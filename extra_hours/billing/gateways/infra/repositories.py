58106b5d178f2d296beaf8c2058e01f445465cef
flaviogf <flavio.fernandes6@gmail.com>
2019-05-27 14:00:29 -0300

    implementation method save firebase user repository billing context

--- extra_hours/billing/gateways/infra/repositories.py
+++ extra_hours/billing/gateways/infra/repositories.py
@@ -1,9 +1,35 @@
+import uuid
+
+from firebase_admin import firestore
+from firebase_admin.auth import get_user, AuthError
+
 from extra_hours.billing.entities import User
 
 
 class FirebaseUserRepository:
     def find_by_id(self, uid):
-        return User(uid=uid)
+        try:
+            user_record = get_user(str(uid))
+        except (AuthError, ValueError):
+            return None
+        else:
+            uid = uuid.UUID(user_record.uid)
+            user = User(uid=uid)
+            return user
 
     def save(self, user):
-        pass
+        db = firestore.client()
+
+        user_collection = db.collection('user')
+
+        user_document = user_collection.document(str(user.uid))
+
+        billing_collection = user_document.collection('billing')
+
+        for billing in user.billing:
+            billing_document = billing_collection.document(str(billing.uid))
+
+            billing_document.set({
+                'title': billing.title,
+                'value': billing.value
+            })
--- tests/billing/gateways/infra/test_repositories.py
+++ tests/billing/gateways/infra/test_repositories.py
@@ -1,15 +1,30 @@
-import unittest
 import uuid
+from datetime import datetime
 
-from extra_hours.billing.entities import User
+from firebase_admin.auth import create_user, list_users, delete_user
+
+from extra_hours.billing.entities import User, Billing
 from extra_hours.billing.gateways.infra.repositories import FirebaseUserRepository
+from extra_hours.billing.value_objects import BillingSummary
+from tests.billing.gateways.infra.base import InfraTestCase
 
 
-class FirebaseUserRepositoryTests(unittest.TestCase):
+class FirebaseUserRepositoryTests(InfraTestCase):
     def setUp(self):
         self._steve_id = uuid.uuid4()
 
         self._steve = User(uid=self._steve_id)
+
+        self._yesterday = datetime(year=2019, month=6, day=13)
+
+        self._billing_summary = BillingSummary(title='Gas station security',
+                                               description='Gas station security yesterday',
+                                               value=100.50,
+                                               work_date=self._yesterday)
+
+        self._billing = Billing(summary=self._billing_summary)
+
+        create_user(uid=str(self._steve.uid), email='steve@email.com', password='test123')
 
         self._user_repository = FirebaseUserRepository()
 
@@ -18,5 +33,16 @@ class FirebaseUserRepositoryTests(unittest.TestCase):
 
         self.assertIsInstance(user, User)
 
+    def test_should_find_by_id_return_none_when_user_not_exists(self):
+        user = self._user_repository.find_by_id(uuid.uuid4())
+
+        self.assertIsNone(user)
+
     def test_should_save_user(self):
+        self._steve.add_billing(self._billing)
+
         self._user_repository.save(self._steve)
+
+    def tearDown(self):
+        for user in list_users().iterate_all():
+            delete_user(user.uid)
